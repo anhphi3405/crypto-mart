51a776a2e1aa57371a171fae1b21ee02998c84f1
phipham-304 <pp9373334@gmail.com>
2025-02-21 18:06:52 +0700

    connect wallet

---
+++ src/components/header/header.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import { CardanoWallet } from '@meshsdk/react';
+import Link from 'next/link';
+export default function Header() {
+  return (
+    <div style={{height : '100px', display : 'grid', gridTemplateColumns : '2fr 8fr', borderBottom : '1px solid #e0e0e0'}}>
+      <div style={{display : 'grid', alignItems : 'center'}}>
+        {/* <h1 style={{fontSize : '24px', backgroundImage : 'linear-gradient(to right, rgb(198, 28, 198), rgb(225, 13, 48))',
+            WebkitBackgroundClip: 'text',
+            backgroundClip: 'text',
+            color: 'transparent',
+            fontWeight : 'bold',
+            textAlign : 'center'
+        }}> Corner Market </h1> */}
+        <img src="https://static.vecteezy.com/system/resources/previews/008/635/894/non_2x/market-crypto-logo-vector.jpg" alt="" style={{width : '60px', height : '60px',  marginLeft : 'calc(50% - 40px)'}}/>
+      </div>
+      <div style={{display : 'flex', justifyContent :'flex-end', alignItems : 'center', paddingRight : '5%', color : "blue", gap: '20px'}}>
+        <Link href="/home">Home</Link>
+        <Link href="/product">Products</Link>
+        <CardanoWallet></CardanoWallet>
+      </div>
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import "@meshsdk/react/styles.css";
 import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { MeshProvider } from "@meshsdk/react";
+import Header from "@/components/header/header";
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MeshProvider>
+      <Header></Header>
+      <Component {...pageProps} />
+    </MeshProvider>
+  );
 }
+
+export default MyApp;
